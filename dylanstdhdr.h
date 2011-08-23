e55ac644d9cb9a883960c1d59ff2caa73d97abb3
Dylan Hall <dylan.p.hall@gmail.com>
2011-08-23 15:15:23 -0500

    Added dylandstdhdr.h

---
+++ dylanstdhdr.h
@@ -0,0 +1,34 @@
+/**
+ * Dylan Hall
+ * dylanphall
+ *
+ * dylan.p.hall@gmail.com
+ *
+ * dylanstdhdr.h
+ *
+ *
+ */
+
+#ifndef DYLANSTDHDR_H_
+#define DYLANSTDHDR_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+
+#ifdef DEBUG
+  #define Dd(X) (fprintf(stderr, #X " digit value : %d\n", (X)), (X) )
+  #define Ds(X) (fprintf(stderr, #X " string value : %s\n", (X)), (X) )
+  #define Df(X) (fprintf(stderr, #X " float value : %f\n", (X)), (X) )
+#else
+  #define Df(X) (X) 
+  #define Df(X) (X)
+  #define Df(X) (X)
+#endif
+
+
+#endif
