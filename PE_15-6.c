da7b4841183816c0341f8a4a5fb662b245daf24b
Dylan Hall <dylanphall@cs.uchicago.edu>
2011-08-24 16:39:22 -0500

    Added shell PE_15-6.c

---
+++ PE_15-6.c
@@ -0,0 +1,23 @@
+/**
+ * Dylan Hall
+ * dylanphall
+ *
+ * dylan.p.hall@gmail.com
+ *
+ * PE_15-6.c
+ *
+ * Created: Aug 24 2011 16:38:06
+ *
+ */
+
+
+typedef char * string;
+
+
+
+int main (int argc, char *argv[]) {
+
+
+return 0;
+}
+
