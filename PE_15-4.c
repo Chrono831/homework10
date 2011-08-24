c3aaf7f49b9c7b751f62732abceca4ac3c5774eb
Dylan Hall <dylanphall@cs.uchicago.edu>
2011-08-24 16:06:27 -0500

    PE_15-4.c finalized

--- PE_15-4.c
+++ PE_15-4.c
@@ -17,12 +17,15 @@ typedef char * string;
 void wait(double);
 
 int main (void) {
-  double delay = 1;
-  for(int i = 0; i < 10; i++) {
-    putchar('i');
-    wait(delay);
-  }
-
+  
+  double delay = 0.5;
+  time_t start, finish;
+  start = time(NULL);
+  fprintf(stdout, "Waiting 10 seconds.\nStart time  : %ld\n", start);
+  for(int i = 0; i < 20; i++) { wait(delay); }
+  finish = time(NULL);
+  fprintf(stdout, "Finish time : %ld\n", finish);
+  fprintf(stdout, "Delta time  : %ld\n", (finish-start) );
 
   return 0;
 }
@@ -32,5 +35,4 @@ void wait(double time) {
   clock_t start = clock();
   while ( ( ((double)(clock() - start)) / (CLOCKS_PER_SEC) ) < time )
     ; //wait
-  return;
 }
